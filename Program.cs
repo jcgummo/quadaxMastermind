44c22a3a50ab9477585f99df74a2479fb4bbd68a
jcgummo <50105285+jcgummo@users.noreply.github.com>
2019-04-29 09:15:38 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Linq;
+
+/// <summary>
+/// This class is a C# console application that is a simple version of 
+/// Mastermind.  The randomly generated answer should be four (4) digits in 
+/// length, with each digit between the numbers 1 and 6.  After the player 
+/// enters a combination, a minus (-) sign should be printed for every digit 
+/// that is correct but in the wrong position, and a plus (+) sign should be 
+/// printed for every digit that is both correct and in the correct position.  
+/// Nothing should be printed for incorrect digits.  The player has ten (10) 
+/// attempts to guess the number correctly before receiving a message that they
+/// have lost.
+/// </summary>
+class MainClass {
+
+    const int MIN_NUMBER = 1;
+    const int MAX_NUMBER = 6;
+    const int NUMBER_OF_DIGITS_IN_NUMBER = 4;
+    const int NUMBER_OF_GUESS_ATTEMPTS = 10;
+
+    public static void Main(string[] args)
+    {
+        // DEFINE Variables...
+        Random rnd = new Random();
+        int[] target = new int[NUMBER_OF_DIGITS_IN_NUMBER];
+        string[] guessedResults = new string[NUMBER_OF_DIGITS_IN_NUMBER];
+        bool playGameNow = true;
+
+        // GENERATES the Number to Guess...
+        while (target.Distinct().Count() != NUMBER_OF_DIGITS_IN_NUMBER)
+        {
+            for (int x = 0; x < NUMBER_OF_DIGITS_IN_NUMBER; x++)
+            {
+                target[x] = rnd.Next(MIN_NUMBER, MAX_NUMBER+1);
+                guessedResults[x] = " ";
+            }
+        }
+
+        // CONCATENATES the individual numeric digits into a single target number string for display purposes...
+        string targetNumber = string.Join("", target.Select(i => i.ToString()).ToArray());
+
+        try
+        {
+            Console.WriteLine("Enter a {0}-digit number, where the numbers are between {1} and {2}:", NUMBER_OF_DIGITS_IN_NUMBER, MIN_NUMBER, MAX_NUMBER);
+
+            int attempts = 0;
+            while (playGameNow)
+            {
+                string userInput = Console.ReadLine();
+                attempts++;
+
+                Console.WriteLine("Attempt #: {0} of {1}", attempts, NUMBER_OF_GUESS_ATTEMPTS);
+
+                int[] userNumber = userInput.Select(v => v - '0').ToArray();
+                int countCorrect = 0;
+                int positionCorrect = 0;
+
+                for (int c = 0; c < NUMBER_OF_DIGITS_IN_NUMBER; c++)
+                {
+                    // VERIFY Number Entered by User Exists in Target Number...
+                    if (target.Contains(userNumber[c]))
+                    {
+                        countCorrect++;
+
+                        if (target[c] == userNumber[c])
+                        {
+                            positionCorrect++;
+                            guessedResults[c] = "+";    // Correct Number, Correct Position
+                        }
+                        else
+                        {
+                            guessedResults[c] = "-";    // Correct Number, Wrong Position
+                        }
+                    }
+                    else
+                    {
+                        guessedResults[c] = " ";        // Wrong Number, Wrong Position
+                    }
+
+                    // CHECK to see if all numbers guessed are in the correct position....
+                    if (positionCorrect == NUMBER_OF_DIGITS_IN_NUMBER)
+                    {
+                        Console.WriteLine("{0}{1}{2}{3}", guessedResults[0], guessedResults[1], guessedResults[2], guessedResults[3]);
+                        Console.WriteLine(" ");
+                        Console.WriteLine(" * * * YOU WON!!! * * *  ");
+                        Console.WriteLine(" You correctly selected the number: {0} in {1} of {2} possible guesses.", targetNumber, attempts, NUMBER_OF_GUESS_ATTEMPTS);
+                        Console.WriteLine(" ");
+                        Console.ReadKey();
+                        playGameNow = false;
+                    }
+                }
+
+                Console.WriteLine("{0}{1}{2}{3}", guessedResults[0] , guessedResults[1] , guessedResults[2] , guessedResults[3]);
+
+                // DETERMINE If Game is over because player failed to guess number in the alloted attempts...
+                if ((attempts >= NUMBER_OF_GUESS_ATTEMPTS) && (playGameNow == true))
+                {
+                    Console.WriteLine(" ");
+                    Console.WriteLine("You LOST!!");
+                    Console.WriteLine("You failed to select the number: {0} in the number of alloted guesses.", targetNumber);
+                    Console.WriteLine(" ");
+                    Console.ReadKey();
+                    playGameNow = false;
+                }
+            }
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("The following error occurred: {0}. Please make sure you entered a valid number. Program will end now.", ex.Message);
+            Console.ReadKey();
+
+            Environment.Exit(10);
+        }
+
+    }
+}
